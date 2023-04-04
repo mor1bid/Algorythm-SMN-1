cbc126fe4769927eb932098f1bbb822299112d31
De <spark49942@gmail.com>
2023-04-04 20:06:47 +0300

    dz1-f

--- dz1.java
+++ dz1.java
@@ -7,7 +7,6 @@ public class dz1
         System.out.println("Введите размер массива:");
         int mylen = work.nextInt();
         Integer ray[] = new Integer[mylen];
-        Integer ay[] = new Integer[]{24, 31, 15, 20, 52, 6};
         Random rnd = new Random();
         for(int i = 0; i < mylen; i++) 
         {
@@ -15,46 +14,10 @@ public class dz1
             ray[i] = num;
         }
         System.out.println(Arrays.asList(ray));
+        System.out.println("Процесс сортировки:");
         pyramidS2(ray);
         work.close();
     }
-
-//     public static void pyramidS1(Integer array[]) 
-//     {
-//         for(int i = 0; i < array.length/2-1; i++)
-//         {
-//             // if(array[i] <= array[2*i+1] && array[i] <= array[2*i+2]) 
-//             // {
-//                 int temp = array[i];
-//                 array[i] = array[2*i+2];
-//                 array[2*i+2] = array[2*i+1];
-//                 array[2*i+1] = temp;
-//             // }
-//         }
-//         System.out.println(Arrays.asList(array));
-//         pyramidS2(array);   
-//     }
-//     public static void pyramidS2(Integer array[]) 
-//     {
-//         boolean inf;
-//         do 
-//         {
-//             inf = true;
-//             for(int i = array.length - 1; i - 1 > 0; i--) 
-//             {
-//                 for(int j = 0; j<array.length -1; j++) 
-//                 {
-//                     int temp = array[i+1];
-//                     array[i+1] = array[j];
-//                     array[j] = temp;
-//                     inf = false;
-//                 }
-//             }
-//         }
-//         while(!inf);
-//         System.out.println(Arrays.asList(array));
-//     }
-// }
 
     public static void pyramidS1(Integer numbers[], int root, int bottom) 
     {
